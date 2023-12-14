dfb23aa51ec84402e208b24341c759e7bf0bb1d9
fillson22 <fillson22051983@gmail.com>
2023-12-14 15:27:48 +0200

    add delete method

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -4,6 +4,23 @@ class DishesController < ApplicationController
     @dish = Dish.new
   end
 
+  def create
+    @dish = Dish.create dish_create_params
+    if @dish.save
+      redirect_to dishes_path
+    end
+  end
+
+  def index
+    @dishes = Dish.all.order created_at: :desc
+  end
+
+  def destroy
+    @dish = Dish.find(params[:id])
+    @dish.destroy
+    redirect_to dishes_path
+  end
+
   private
 
   def dish_create_params
