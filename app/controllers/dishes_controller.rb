9d3d41779f8a523ab342ae55fd378060275ef846
fillson22 <fillson22051983@gmail.com>
2023-12-15 12:22:39 +0200

    add method edit update for dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,11 +1,12 @@
 class DishesController < ApplicationController
+  before_action :set_dish, only: [:show, :destroy, :edit, :update]
 
   def new
     @dish = Dish.new
   end
 
   def create
-    @dish = Dish.create dish_create_params
+    @dish = Dish.create dish_params
     if @dish.save
       redirect_to dishes_path
     end
@@ -15,15 +16,27 @@ class DishesController < ApplicationController
     @dishes = Dish.all.order created_at: :desc
   end
 
+  def show; end
+
   def destroy
-    @dish = Dish.find(params[:id])
     @dish.destroy
     redirect_to dishes_path
   end
 
+  def edit; end
+
+  def update
+    @dish.update dish_params
+    redirect_to dish_path
+  end
+
   private
 
-  def dish_create_params
+  def set_dish
+    @dish = Dish.find(params[:id])
+  end
+
+  def dish_params
     params.require(:dish).permit(:name, :proteins, :fats, :carbohydrates, :cal)
   end
 end
